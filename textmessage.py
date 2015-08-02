da1c7187f3cc8d29a549c7c7e58882e72afa8e5c
yrsegal <yrsegal@gmail.com>
2015-08-02 11:05:39 -0400

    add# now adds a phone number
    rather than adding a job with a phone number
    prevents inter-plugin conflict

--- textmessage.py
+++ textmessage.py
@@ -7,74 +7,24 @@ printer = PluginPrinterInstance()
 printer.setname("Texting")
 printer.setcolor(bcolors.GREEN)
 
-def addWithPhone(**kwargs):
+def addNumberToJob(**kwargs):
 	args, authstate, sec, ws, queue = kwargs["args"], kwargs["authstate"], kwargs["sec"], kwargs["ws"], kwargs["queue"]
-	name, priority, esttime, material, phone = args["name"], args["priority"], args["time"], args["material"], args["phone"]
+	job_uuid, phone = args["uuid"], args["phone"]
 
-	# If the priority or the material aren't set up, or the name isn't defined
-	if not name or material == "N/A" or priority == -1:
-		# Tell the client then don't add the object
-		serveToConnection({
-			"action": "notification",
-			"title": "Incomplete data",
-			"text": "Please fill out the submission form fully."
-			}, ws)
-		if argvs.loud:
-			cprint("Insufficient data to add job to queue.", color=bcolors.YELLOW)
-		return
+	job, masterindex, priority, index = queue.getQueueObject(job_uuid)
 
-	# Contain the length of time within the configurable bounds.
-	bounds = config["length_bounds"]
-	if bounds[0] >= 0:
-		esttime = max(bounds[0], esttime)
-	if bounds[1] >= 0:
-		esttime = min(bounds[1], esttime)
+	if "phone" not in job or job["phone"] == "none":
+		if authstate:
+			job["phone"] = phone
+			job["coachmodified"] = True
+		else:
+			return "Changing a set phone number requires auth."
+	else:
+		job["phone"] = phone
 
-	# lock the priority down to the max if the user isn't authed.
-	if not config["priority_selection"] and not authstate:
-		priority = min(config["default_priority"], priority)
-
-	# Recalculate priority if applicable.
-	if config["recalc_priority"]:
-		priority = _calcpriority(priority, esttime)
-
-	# Strip whitespace from the name and recapitalize it
-	name = name.strip().rstrip()
-	if config["recapitalize"]:
-		name = name.title()
-
-	# Make sure the user isn't in the queue. The config can allow multiple materials per person, or not.
-	inqueue = False
-	for i in queue.queue:
-		for j in i: 
-			if name.lower() == j["name"].lower() and (
-					material == j["material"] or (not config["allow_multiple_materials"])):
-				inqueue = True
-				break
-
-	# Make the uuid of the job
-	job_uuid = str(uuid.uuid1())
-
-	if not inqueue or config["allow_multiples"]: # If the job is allowed to be created
-		# Add it to the queue
-		queue.queue[priority].append(QueueObject({
-			"totaldiff": 0,
-			"priority": priority,
-			"name": name,
-			"material": material,
-			"esttime": esttime,
-			"coachmodified": authstate,
-			"uuid": job_uuid,
-			"sec": sec,
-			"time": time.time(),
-			"phone": phone
-		}))
-
-		if argvs.loud: # If -v, report success
-			color = bcolors.MAGENTA if authstate else ""
-			cprint("Added {} to the queue.\n({})".format(name, job_uuid), color=color)
-	elif argvs.loud: # If -v, report failures
-		cprint("Cannot add {} to the queue.".format(name), color=bcolors.YELLOW)
+	if argvs.loud: # If -v, report success
+		color = bcolors.MAGENTA if authstate else ""
+		printer.cprint("Gave {} a phone number.\n({})".format(job["name"], job_uuid), color=color)
 
 
 prevLength = 0
@@ -116,8 +66,9 @@ hideFromClient = [
 ]
 
 socketCommands = [
-	SocketCommand("add#", addWithPhone, {"name": str, "priority": int,
-	 "time": any_number, "material": str, "phone": str})
+	SocketCommand("add#", addNumberToJob, {"uuid": str, "phone": str})
 ]
 
+requiresAuth = [
+]
 
